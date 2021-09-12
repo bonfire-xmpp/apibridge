0d64d60971ac537fb1e1c18365c3a6cfd394886d
00drip <rxsunshine@protonmail.com>
2021-09-12 00:07:10 -0400

    Add function errors and event wildcards

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const EVENT     = 0,
       FN_CALL   = 1,
-      FN_RETURN = 2;
+      FN_RETURN = 2,
+      FN_THROW  = 3;
 
 class Bridge {
   id = 0;
@@ -30,8 +31,8 @@ class Bridge {
     get(target, fn) {
       return (...args) => {
         const curid = target.id++;
-        const p = new Promise(resolve => {
-          target.promises[curid] = resolve;
+        const p = new Promise((resolve, reject) => {
+          target.promises[curid] = { resolve, reject };
         });
         target.onsend([FN_CALL, curid, fn, args]);
         return p;
@@ -54,25 +55,41 @@ class Bridge {
         for (let i = 0; i < (this.listeners[e]?.length ?? 0); ++i) {
           this.listeners[e][i](payload);
         }
+        if (this.listeners["*"]?.length) {
+          for (let i = 0; i < this.listeners["*"].length; ++i) {
+            this.listeners["*"][i](e, payload);
+          }
+        }
         break;
       };
 
       case FN_CALL: {
         const [, id, fn, args] = frame;
-        this.onsend([FN_RETURN, id, await this.functions[fn](...args)]);
+        try {
+          this.onsend([FN_RETURN, id, await this.functions[fn](...args)]);
+        } catch (e) {
+          this.onsend([FN_THROW, id, e]);
+        }
         break;
       };
 
       case FN_RETURN: {
         const [, id, retval] = frame;
-        this.promises[id](retval);
+        this.promises[id].resolve(retval);
         delete this.promises[id];
         break;
       };
+
+      case FN_THROW: {
+        const [, id, error] = frame;
+        this.promises[id].reject(error);
+        delete this.promises[id];
+        break;
+      }
     }
   }
 
-  onsend = mesg => {};
+  onsend = () => {};
 }; 
 
 module.exports = { Bridge };
