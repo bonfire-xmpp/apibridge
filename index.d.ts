fbbce45e8dcc4d2083a5a79a119e27f7d6e47473
00drip <rxsunshine@protonmail.com>
2021-09-29 12:38:23 -0400

    feat: 🎸 improve typescript declaration

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 declare type EventCallback = (arg: any) => any;
 declare type AsyncFunction = (...args: any[]) => Promise<any>;
 
-export declare class Bridge<Remote> {
-  public on(event: string, cb: EventCallback): void;
-  public once(event: string, cb: EventCallback): void;
-  public off(event: string, cb: EventCallback): void;
-  public emit(event: string, mesg: any): void;
-  public fn: Remote;
-  public define(funcs: { [funcname: string]: AsyncFunction });
+export declare class Bridge<RemoteFuncs = any, RemoteEvents = any, LocalFuncs = any, LocalEvents = any> {
+  public on(event: RemoteEvents, cb: EventCallback): void;
+  public once(event: RemoteEvents, cb: EventCallback): void;
+  public off(event: RemoteEvents, cb: EventCallback): void;
+  public emit(event: LocalEvents, mesg: any): void;
+  public fn: RemoteFuncs;
+  public define(funcs: LocalFuncs);
 
   public feed(frame: Array<any>): Promise<void>;
   public onsend: (mesg: any) => void;
