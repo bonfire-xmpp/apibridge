a9ad7da61f3621f143301b36bc0f3abc68095e84
rxcommit <rxsunshine@protonmail.com>
2021-10-04 14:20:25 -0400

    flatten function structure

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const EVENT     = 0,
 function callPromise(bridge, path) {
   const inner = (path) => new Proxy(function() {}, {
     get(_target, prop) {
-      return inner(path.concat([prop]));
+      return inner(path.length ? path + "." + prop : prop);
     },
     apply(_target, _thisarg, args) {
       const curid = bridge.id++;
@@ -18,6 +18,27 @@ function callPromise(bridge, path) {
     },
   });
   return inner(path);
+}
+
+function flatten(obj) {
+  const stack = [[obj, []]];
+  const flatobj = {};
+  while (stack.length) {
+    const top = stack.pop();
+    for (const key in top[0]) {
+      switch (typeof top[0][key]) {
+        case "object": {
+          stack.push([top[0][key], top[1].concat([key])]);
+          break;
+        };
+        case "function": {
+          flatobj[top[1].concat([key]).join(".")] = top[0][key];
+          break;
+        };
+      }
+    }
+  }
+  return flatobj;
 }
 
 class Bridge {
@@ -51,7 +72,7 @@ class Bridge {
   }
 
   define(fns) {
-    this.functions = fns;
+    this.functions = flatten(fns);
   }
 
   async feed(frame) {
@@ -72,7 +93,7 @@ class Bridge {
       case FN_CALL: {
         const [, id, fn, args] = frame;
         try {
-          this.onsend([FN_RETURN, id, await fn.reduce((cur, key) => cur[key], this.functions)(...args)]);
+          this.onsend([FN_RETURN, id, await this.functions[fn](...args)]);
         } catch (e) {
           this.onsend([FN_THROW, id, e]);
         }
